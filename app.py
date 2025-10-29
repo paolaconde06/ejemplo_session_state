1e7849b59986fd84a70085c4e0571761d0a429d0
paolaconde06 <a374460@uach.mx>
2025-10-29 08:11:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ if "count" not in st.session_state:
   st.session_state["count"]=0
 
 if st.button("Click me"):
-  st.session_state("count")==1
+  st.session_state["count"]==1
 
 st.write(st.session_state)
